89f9010a4b2b1f02ad7725d516826681f09e17de
vas-alina <vasalina93@gmail.com>
2024-12-06 11:31:46 +0300

    Убран старый код

--- client/src/components/cart-button/Cart-button.jsx
+++ client/src/components/cart-button/Cart-button.jsx
@@ -1,35 +1,4 @@
-// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import { useDispatch, useSelector } from "react-redux";
-// import { selectCart } from "../../selectors";
-// import { addProductToCart, removeFromCartAsync } from "../../action";
 
-// export const CartButton = ({ product, size = 24 }) => {
-//   const dispatch = useDispatch();
-//   const cart = useSelector(selectCart);
-//   console.log(cart);
-//   const productId = product.id;
-
-//   const isCart = cart.some((item) => item.id === productId);
-
-//   const handleToogleToCart = () => {
-//     const count = 1;
-//     if (isCart) {
-//       dispatch(removeFromCartAsync(productId));
-//     } else {
-//       dispatch(addProductToCart(productId, count));
-//     }
-//   };
-
-//   return (
-//     <div onClick={handleToogleToCart} style={{ cursor: "pointer" }}>
-//       {isCart ? (
-//         <ShoppingCartIcon style={{ color: "red", fontSize: `${size}px` }} />
-//       ) : (
-//         <ShoppingCartIcon style={{ color: "grey", fontSize: `${size}px` }} />
-//       )}
-//     </div>
-//   );
-// };
 import { useDispatch, useSelector } from "react-redux";
 import { useMemo } from "react";
 import { addProductToCart } from "../../action";
